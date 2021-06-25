6349d50b59a65aa4b518876a00d10518a7f9f613
smindl <yasmineludwig@gmail.com>
2021-06-25 11:28:59 +0200

    added home component

--- webfr-projektarbeit/src/app/app-routing.module.ts
+++ webfr-projektarbeit/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { FaqComponent } from './faq/faq.component';
@@ -8,6 +9,7 @@ import { ProfileComponent } from './profile/profile.component';
 
 
 const routes: Routes = [
+  {path: "home", component: HomeComponent},
   {path: "login", component: LoginComponent},
   {path: "sign-up", component: SignUpComponent},
   {path: "faq", component: FaqComponent},
--- webfr-projektarbeit/src/app/app.module.ts
+++ webfr-projektarbeit/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -28,7 +29,8 @@ import { FaqComponent } from './faq/faq.component';
     SignUpComponent,
     HighscoreComponent,
     ProfileComponent,
-    FaqComponent
+    FaqComponent,
+    HomeComponent
   ],
   imports: [
     ReactiveFormsModule,
