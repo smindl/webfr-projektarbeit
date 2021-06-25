5f316172677d65a4c771405c82f370728739eaa0
jacob-sch <jacob.schmauss@gmail.com>
2021-06-25 11:07:44 +0200

    routing
    no styling just the buttons

--- webfr-projektarbeit/src/app/app-routing.module.ts
+++ webfr-projektarbeit/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { FaqComponent } from './faq/faq.component';
+import { HighscoreComponent } from './highscore/highscore.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "login", component: LoginComponent},
+  {path: "sign-up", component: SignUpComponent},
+  {path: "faq", component: FaqComponent},
+  {path: "highscore", component: HighscoreComponent},
+  {path: "profile", component: ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
