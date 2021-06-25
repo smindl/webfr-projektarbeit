5b544d6218393995b8af4bf143ba902bb00c7a26
smindl <yasmineludwig@gmail.com>
2021-06-25 11:47:45 +0200

    added name from home to game

--- webfr-projektarbeit/src/app/app-routing.module.ts
+++ webfr-projektarbeit/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { FaqComponent } from './faq/faq.component';
 import { HighscoreComponent } from './highscore/highscore.component';
 import { ProfileComponent } from './profile/profile.component';
+import { GameComponent } from './game/game.component';
 
 
 const routes: Routes = [
-  {path: "home", component: HomeComponent},
+  {path: "game", component: GameComponent},
   {path: "login", component: LoginComponent},
   {path: "sign-up", component: SignUpComponent},
   {path: "faq", component: FaqComponent},
--- webfr-projektarbeit/src/app/game/game.component.spec.ts
+++ webfr-projektarbeit/src/app/game/game.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { GameComponent } from './game.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('GameComponent', () => {
+  let component: GameComponent;
+  let fixture: ComponentFixture<GameComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ GameComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(GameComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
