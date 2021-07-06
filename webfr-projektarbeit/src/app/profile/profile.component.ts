698f6decbf15dcfe828a58afe7d6d496f49ca5bc
jacob-sch <jacob.schmauss@gmail.com>
2021-07-06 16:59:08 +0200

    backend for profile page

--- webfr-projektarbeit/src/app/profile/profile.component.ts
+++ webfr-projektarbeit/src/app/profile/profile.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+
+
+export interface ProfileData {
+  email : string,
+  username: string,
+  highscore : Number,
+  company : String,
+  street : String,
+  city : String,
+  postcode : Number
+}
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +19,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  httpOptions = {
+    headers : new HttpHeaders({ 'Content-Type':'application/json'})
+  };
+
+  profileData : ProfileData;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    //hard coded username will be replaced by active user when finished
+    let user = {username : "test0r"}
+
+    this.http.post<{ data: ProfileData}>("http://localhost:3000/profile",user, this.httpOptions).subscribe({
+      next: (responseData) => {
+        this.profileData = responseData.data
+        console.log(this.profileData)
+      },
+      error: (err) => {
+        // do something with the error
+      },
+    });
+    
   }
 
 }
