92663937d5a55a23bc464fdc2765e8acbedcba30
smindl <yasmineludwig@gmail.com>
2021-07-08 14:45:32 +0200

    navbar change

--- webfr-projektarbeit/src/app/navbar/navbar.component.ts
+++ webfr-projektarbeit/src/app/navbar/navbar.component.ts
@@ -8,16 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   public navbarCollapsed = true;
 
-  visability = sessionStorage.getItem('login')
+  //visability = sessionStorage.getItem('login')
 
   constructor() { }
 
   logout() : void {
     sessionStorage.clear();
   }
-  
-  ngOnInit(): void {
-    document.getElementById("logout")?.addEventListener("click", this.logout);  
+
+  userIsLoggedIn() : boolean {
+    //document.getElementById("logout")?.addEventListener("click", this.logout); 
+    return sessionStorage.getItem("login") == "true" ? true : false;
   }
+  
+  ngOnInit(): void {}
 
 }
